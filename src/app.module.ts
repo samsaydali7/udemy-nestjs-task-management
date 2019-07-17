196d49476563f8b713b873b59b912342de09033f
Samer Alsaydali <samsaydali@gmail.com>
2019-07-17 13:55:42 +0300

    Auth setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { typeOrmConfig, typeOrmMysqlConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmMysqlConfig),
-    TasksModule],
+    TasksModule,
+    AuthModule],
   controllers: [],
   providers: [],
 })
